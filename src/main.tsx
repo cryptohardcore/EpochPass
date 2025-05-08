49bcb2897f6467941df4624cc5b2525c6f898826
cryptohardcore <133330042+cryptohardcore@users.noreply.github.com>
2025-05-08 20:41:51 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
-import { WalletProvider } from './contexts/WalletContext';
+import { WalletProvider } from './contexts/WalletContext.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
